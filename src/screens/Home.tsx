a0e1da0f115aaa660de5062cfac80416fe2d1726
RubenSouza <rubem.eliel20212@gmail.com>
2022-09-27 00:05:02 -0300

    backup commit

---
+++ src/screens/Home.tsx
@@ -0,0 +1,131 @@
+import { useState } from "react";
+import auth from "@react-native-firebase/auth";
+import { Alert } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  HStack,
+  IconButton,
+  VStack,
+  useTheme,
+  Text,
+  Heading,
+  FlatList,
+  Center,
+} from "native-base";
+import { SignOut, ChatTeardropText } from "phosphor-react-native";
+import Logo from "../assets/logo_secondary.svg";
+import { Filter } from "../components/Filter";
+import { Button } from "../components/Button";
+import { Order, OrderProps } from "../components/Order";
+
+export function Home() {
+  const [statusSelected, setStatusSelected] = useState<"open" | "closed">(
+    "open"
+  );
+  const [orders, setOrders] = useState<OrderProps[]>([
+    {
+      id: "123",
+      patrimony: "123456",
+      when: "18/07/2022 às 19:00",
+      status: "open",
+    },
+  ]);
+  const navigation = useNavigation();
+  const { colors } = useTheme();
+
+  function handleNewOrder() {
+    navigation.navigate("new");
+  }
+
+  function handleOpenDetails(orderId: string) {
+    navigation.navigate("details", { orderId });
+  }
+
+  function handleLogout() {
+    auth()
+      .signOut()
+      .catch(error => {
+        console.log(error);
+        return Alert.alert("Sair", "Não foi possível sair");
+      });
+  }
+
+  return (
+    //Container
+    <VStack flex={1} pb={6} bg="gray.700">
+      {/*Logo e SignOut*/}
+      <HStack
+        w={"full"}
+        justifyContent="space-between"
+        alignItems={"center"}
+        bg="gray.600"
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+          onPress={handleLogout}
+        />
+      </HStack>
+      {/* Título e Número de Chamadas */}
+      <VStack flex={1} px={6}>
+        <HStack
+          w={"full"}
+          mt={8}
+          mb={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading color={"gray.100"}>Solicitações</Heading>
+          <Text color={"gray.200"}>{orders.length}</Text>
+        </HStack>
+        {/* Filtros */}
+        <HStack space={3} mb={8}>
+          <Filter
+            type="open"
+            title="em andamento"
+            onPress={() => {
+              setStatusSelected("open");
+            }}
+            isActive={statusSelected === "open"}
+          />
+          <Filter
+            type="closed"
+            title="finalizados"
+            onPress={() => {
+              setStatusSelected("closed");
+            }}
+            isActive={statusSelected === "closed"}
+          />
+        </HStack>
+        <FlatList
+          data={orders}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Order data={item} onPress={() => handleOpenDetails(item.id)} />
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={() => (
+            <Center>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
+              <Text
+                color={"gray.300"}
+                fontSize={"xl"}
+                mt={6}
+                textAlign="center"
+              >
+                Você ainda não possui {"\n"}
+                solicitações{" "}
+                {statusSelected === "open" ? "em andamento" : "finalizados"}
+              </Text>
+            </Center>
+          )}
+        />
+        <Button title="Nova Solicitação" onPress={handleNewOrder} />
+      </VStack>
+    </VStack>
+  );
+}
